8739dd3b0d7265556b3842d20f6c36c65d91d7fa
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-09-16 12:53:37 +0530

    otp ajax

--- application/controllers/ajax/Index.php
+++ application/controllers/ajax/Index.php
@@ -382,9 +382,10 @@ class Index extends CC_Controller
 		
 		$settingsdetail = $this->Systemsettings_Model->getList('row');
 		if($settingsdetail && $settingsdetail['otp']=='0'){
-			echo $otp;//////////
+			$this->sms(['no' => $mobile, 'msg' => 'One Time Password is '.$otp, 'userid' => $userdata['id'], 'email' => $userdata['email'], 'smsenable' => '0']);
+			echo $otp;
 		}else{
-			$this->sms(['no' => $mobile, 'msg' => 'One Time Password is '.$otp, 'userid' => $userdata['id'], 'email' => $userdata['email']]);
+			$this->sms(['no' => $mobile, 'msg' => 'One Time Password is '.$otp, 'userid' => $userdata['id'], 'email' => $userdata['email'], 'smsenable' => '1']);
 			echo '';
 		}
 	}
