0940d2a67d8910337c5d7cb61877043d870f9034
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-11-03 15:25:42 +0530

    lms sidebar

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -324,6 +324,18 @@ if(count($permission) > 0){
 									</div>
 								</div>
 							</li>
+							<?php if ($registrationno !='') { ?>
+							<li>
+								<div class="row side_list">
+									<div class="col-sm-3 col-md-3 col-lg-3 list_icon">
+										<a href="<?php echo base_url().'plumber/lms/index'; ?>"><i class="fa fa-book" aria-hidden="true"></i></a>
+									</div>
+									<div class="col-sm-9 col-md-9 col-lg-9 list_name">
+										<a href="<?php echo base_url().'plumber/lms/index'; ?>" class="cus_li_name"><p class="custom_li">LMS</p></a>
+									</div>
+								</div>
+							</li>
+							<?php } ?>
 							<li>
 								<div class="row side_list">
 									<div class="col-sm-3 col-md-3 col-lg-3 list_icon">
