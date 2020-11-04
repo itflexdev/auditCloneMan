2ba5563bc415f9f20a5463b289693dead0c9c281
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-11-04 13:07:30 +0530

    auditor diary menu

---
+++ application/views/common/auditor/menu.php
@@ -0,0 +1,10 @@
+
+
+<div class="card-body">
+	<div class="col-md-12 breadcrumb_tab">
+		<a href="<?php echo base_url().'admin/audits/index/action/'.$id; ?>" class="stepbar">Auditors Profile</a>
+		
+		<a href="<?php echo base_url().'admin/audits/index/diary/'.$id; ?>" class="stepbar">Diary/Comments</a>
+	</div>
+</div>
+		
