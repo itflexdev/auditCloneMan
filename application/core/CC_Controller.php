b386c95ccc0cabbd21ab6de4a3ba3727d2c5e956
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-11-05 20:08:43 +0530

    cpd cc

--- application/core/CC_Controller.php
+++ application/core/CC_Controller.php
@@ -1143,7 +1143,7 @@ class CC_Controller extends CI_Controller
 		}
 	}
 
-	public function pdfelectroniccocreport($id, $userid)
+	/*public function pdfelectroniccocreport($id, $userid)
 	{		
 		$userdata				 		= $this->Plumber_Model->getList('row', ['id' => $userid], ['users', 'usersdetail', 'usersplumber']);
 		$pagedata['userdata']	 		= $userdata;
@@ -1156,6 +1156,36 @@ class CC_Controller extends CI_Controller
 		$pagedata['specialisations']	= $this->Installationtype_Model->getList('all', ['ids' => ['4','8']]);
 
 		$html = $this->load->view('pdf/electroniccocreport', (isset($pagedata) ? $pagedata : ''), true);
+		$this->pdf->loadHtml($html);
+		$this->pdf->setPaper('A4', 'portrait');
+		$this->pdf->render();
+		$output = $this->pdf->output();
+		$this->pdf->stream('Electronic COC Report '.$id);
+	}*/
+
+	public function pdfelectroniccocreport($id, $userid)
+	{		
+		$userdata				 		= $this->Plumber_Model->getList('row', ['id' => $userid], ['users', 'usersdetail', 'usersplumber', 'coclog', 'coclogprovince', 'coclogcity', 'coclogsuburb', 'coclogcompany']);
+
+		$pagedata['userdata']	 		= $userdata;
+		$pagedata['specialisations']	= explode(',', $pagedata['userdata']['specialisations']);
+		$pagedata['result']		    	= $this->Coc_Model->getCOCList('row', ['id' => $id], ['coclog', 'coclogprovince', 'coclogcity', 'coclogsuburb', 'coclogcompany', 'users', 'usersdetail']);
+		$pagedata['noncompliance'] 	= $this->Noncompliance_Model->getList('all', ['coc_id' => $id, 'user_id' => $userid]);
+		$pagedata['designation2'] 		= $this->config->item('designation2');
+		$specialisations 				= explode(',', $userdata['specialisations']);
+		$pagedata['installationtype']	= $this->getInstallationTypeList();
+		$pagedata['installation'] 		= $this->Installationtype_Model->getList('all', ['ids' => ['1','2','3','5','6','7']]);
+		$pagedata['specialisations']	= $this->Installationtype_Model->getList('all', ['ids' => ['4','8']]);
+
+
+		$noncompliance					= $this->Noncompliance_Model->getList('all', ['coc_id' => $id, 'user_id' => $userid]);
+		if (count($noncompliance) > 0) {
+			$pagedata['noncompliance'] 	= $this->Noncompliance_Model->getList('all', ['coc_id' => $id, 'user_id' => $userid]);
+			$html = $this->load->view('pdf/electroniccoc_ncreport', (isset($pagedata) ? $pagedata : ''), true);
+		}else{
+			$html = $this->load->view('pdf/electroniccocreport', (isset($pagedata) ? $pagedata : ''), true);
+		}
+		
 		$this->pdf->loadHtml($html);
 		$this->pdf->setPaper('A4', 'portrait');
 		$this->pdf->render();
