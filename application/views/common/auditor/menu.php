1c6237358538cef0a7d8e482f381ac2dd48fa64e
ManikandanRJSM <rmanikandanrjsm@outlook.com>
2020-11-06 19:43:36 +0530

    audit menu

--- application/views/common/auditor/menu.php
+++ application/views/common/auditor/menu.php
@@ -3,7 +3,7 @@
 <div class="card-body">
 	<div class="col-md-12 breadcrumb_tab">
 		<a href="<?php echo base_url().'admin/audits/index/action/'.$id; ?>" class="stepbar">Auditors Profile</a>
-		
+		<a href="<?php echo base_url().'admin/audits/index/audithistory/'.$id; ?>" class="stepbar">Audit History</a>
 		<a href="<?php echo base_url().'admin/audits/index/diary/'.$id; ?>" class="stepbar">Diary/Comments</a>
 	</div>
 </div>
