95c7717ec65a6b33a53dd203e4f70242d3cd0802
manikandanRJSM <rmanikandanrjsm@outlook.com>
2021-01-13 11:53:45 -0800

    company profile controller

--- application/controllers/company/profile/Index.php
+++ application/controllers/company/profile/Index.php
@@ -6,6 +6,7 @@ class Index extends CC_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('Documentsletters_Model');
 	}
 	
 	public function index()
@@ -13,5 +14,28 @@ class Index extends CC_Controller
 		$id = $this->getUserID();
 		$this->companyprofile($id, ['roletype' => $this->config->item('rolecompany'), 'pagetype' => 'companyprofile'], ['redirect' => 'company/profile/index']);
 	}
+
+    public function Deletefunc()
+    {       
+        $post = $this->input->post();
+        $result = $this->Documentsletters_Model->deleteid_comp($post['id']);
+       	echo "1";
+    }
+
+    public function actionDocuments(){
+    	$post = $this->input->post();
+    	$data = $this->Documentsletters_Model->action2($post);
+
+    	$documentlist			= $this->Documentsletters_Model->getcompanyList('row', ['id' => $data]);
+
+    	if($documentlist){
+			$json = ['status' => '1', 'result' => $documentlist];
+		}else{
+			$json = ['status' => '0'];
+		}
+
+    	echo json_encode($json);
+
+    }
 	
 }
