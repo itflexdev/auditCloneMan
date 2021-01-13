22f874158c789d8d956d98fb32e38bc08d27be20
manikandanRJSM <rmanikandanrjsm@outlook.com>
2021-01-13 12:02:34 -0800

    company sidebar

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -405,6 +405,9 @@ if(count($permission) > 0){
 					<?php if($formstatus=='1'){ ?>
 						<li><a href="<?php echo base_url().'company/profile/index'; ?>">My Profile</a></li>
 						<li><a href="<?php echo base_url().'company/employee_listing'; ?>">Employee Listing</a></li>
+						<li><a href="<?php echo base_url().'company/performancedetails/index'; ?>">Performance Details</a></li>
+						<li><a href="<?php echo base_url().'company/documents/index'; ?>">Documents and Letters</a></li>
+						
 					<?php }elseif($formstatus=='0'){ ?>
 						<li><a href="<?php echo base_url().'company/registration/index'; ?>">My Profile</a></li>
 					<?php } ?>
