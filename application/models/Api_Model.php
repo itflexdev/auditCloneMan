3e744d6b87995dd6c3b71edbe9e89296a67cf210
manikandanRJSM <rmanikandanrjsm@outlook.com>
2021-01-30 11:51:21 -0800

    api model

--- application/models/Api_Model.php
+++ application/models/Api_Model.php
@@ -489,6 +489,306 @@ class Api_Model extends CC_Model
 		return $result;		
 	}
 
+	public function AuditorgetList($type, $requestdata=[])
+	{ 		
+		$user 			= ['u.id as id', 'u.email','u.type','u.status as usstatus', 'u.password_raw'];
+		$usersdetail 	= ['ud.id as userdetailid','ud.name','ud.surname','ud.company_name','ud.reg_no','ud.vat_no','ud.vat_vendor','ud.billing_email','ud.billing_contact','ud.mobile_phone','ud.work_phone','ud.file1','ud.file2','ud.identity_no'];		
+		$useraddress 	= ['ua.id as useraddressid', 'ua.address', 'ua.province', 'ua.city', 'ua.suburb', 'ua.postal_code'];
+
+		$userbank 		= ['ub.id as userbankid', 'ub.bank_name', 'ub.branch_code', 'ub.account_name', 'ub.account_no', 'account_type'];
+		$auditor 		= ['ub1.id as available', 'ub1.user_id', 'ub1.allocation_allowed', 'ub1.status'];
+
+		$this->db->select('
+			'.implode(',', $user).',
+			'.implode(',', $usersdetail).',
+			'.implode(',', $useraddress).',
+			'.implode(',', $auditor).',
+			'.implode(',', $userbank).',
+			group_concat(concat_ws("@@@", uaa.id, uaa.province, uaa.city, uaa.suburb, p.name, c.name, s.name, uaa.city) separator "@-@") as areas'
+		);
+		$this->db->from('users as u');
+		$this->db->join('users_detail as ud','ud.user_id=u.id', 'left');
+		$this->db->join('users_address as ua', 'ua.user_id=u.id and ua.type="3"', 'left');		
+		$this->db->join('users_bank as ub', 'ub.user_id=u.id', 'left');
+		$this->db->join('auditor_availability as ub1', 'ub1.user_id=u.id', 'left');
+		$this->db->join('users_auditor_area as uaa', 'uaa.user_id=u.id', 'left');
+		$this->db->join('province as p', 'p.id=uaa.province', 'left');
+		$this->db->join('city as c', 'c.id=uaa.city', 'left');
+		$this->db->join('suburb as s', 's.id=uaa.suburb', 'left');
+		
+		if(isset($requestdata['id'])) 			$this->db->where('u.id', $requestdata['id']);
+		//if(isset($requestdata['status'])) 		$this->db->where_in('u.status', $requestdata['status']);
+
+		if($type=='count'){
+			$result = $this->db->count_all_results();
+		}
+		else
+		{
+			$query = $this->db->get();
+			
+			if($type=='all') 		$result = $query->result_array();
+			elseif($type=='row') 	$result = $query->row_array();
+		}
+		
+		return $result;	
+	}
+
+	public function AuditorAreagetList($type, $requestdata=[])
+	{
+		$this->db->select('uaa.*');
+		$this->db->from('users_auditor_area as uaa');
+
+		if(isset($requestdata['id'])) 			$this->db->where('uaa.id', $requestdata['id']);
+		if(isset($requestdata['province'])) 	$this->db->where('uaa.province', $requestdata['province']);
+		if(isset($requestdata['city'])) 		$this->db->where('uaa.city', $requestdata['city']);
+		if(isset($requestdata['suburb'])) 		$this->db->where('uaa.suburb', $requestdata['suburb']);
+		if(isset($requestdata['userid'])) 		$this->db->where('uaa.user_id', $requestdata['userid']);
+
+		if($type=='count'){
+			$result = $this->db->count_all_results();
+		}
+		else
+		{
+			$query = $this->db->get();
+			
+			if($type=='all') 		$result = $query->result_array();
+			elseif($type=='row') 	$result = $query->row_array();
+		}
+		
+		return $result;	
+	}
+
+	public function auditorAction($data)
+	{
+		$this->db->trans_begin();
+
+		$datetime		= 	date('Y-m-d H:i:s');
+		$id				= 	$data['user_id'];
+
+		if(isset($data['email'])) 				$request1['email'] 				= $data['email'];
+		if(isset($data['password'])) 			$request1['password_raw'] 		= $data['password'];
+		if(isset($data['password'])) 			$request1['password'] 			= md5($data['password']);
+		if(isset($data['status'])) 				$request1['status'] 			= $data['status'];		
+
+		
+		if(isset($request1)){
+			if($id==''){
+				$request1['type']	 		= '5';
+				$request1['created_at']		= 	date('Y-m-d H:i:s');
+
+				$userdata = $this->db->insert('users', $request1);
+				$id = $this->db->insert_id();
+			}else{
+				$request1['updated_at']		= 	date('Y-m-d H:i:s');
+				$userdata = $this->db->update('users', $request1, ['id' => $id]);
+			}
+		}
+
+		if(isset($id)) 							$request0['user_id'] 			= $id;
+		if(isset($data['allowed'])) 			$request0['allocation_allowed']	= $data['allowed'];
+		if(isset($data['auditstatus'])) 			$request0['status'] 			= $data['auditstatus'];
+
+		if (isset($request0)) {
+			$auditoravaid			= $data['auditoravaid'];
+			if($auditoravaid==''){
+				$request0['created_at'] 		= $datetime;
+				$auditoravaid1 = $this->db->insert('auditor_availability', $request0);
+			}else{
+				$request0['updated_at'] 		= $datetime;
+				$auditoravaid1 = $this->db->update('auditor_availability', $request0, ['id' => $auditoravaid]);
+			}
+		}
+
+		
+		if(isset($data['name'])) 				$request2['name'] 				= $data['name'];
+		if(isset($data['surname'])) 			$request2['surname'] 			= $data['surname'];
+		if(isset($data['company_name'])) 		$request2['company_name'] 		= $data['company_name'];
+		if(isset($data['reg_no'])) 				$request2['reg_no'] 			= $data['reg_no']; 
+		if(isset($data['vat_no'])) 				$request2['vat_no'] 			= $data['vat_no'];
+
+		$request2['vat_vendor'] 				= isset($data['vat_vendor']) ? $data['vat_vendor'] : '0';
+		
+		if(isset($data['billing_email'])) 		$request2['billing_email'] 		= $data['billing_email'];
+		if(isset($data['billing_contact'])) 	$request2['billing_contact'] 	= $data['billing_contact'];		
+		if(isset($data['work_phone'])) 			$request2['work_phone'] 		= $data['work_phone'];
+		if(isset($data['mobile_phone'])) 		$request2['mobile_phone'] 		= $data['mobile_phone'];	
+		if(isset($data['file1'])) 				$request2['file1'] 				= $data['file1'];
+		if(isset($data['file2'])) 				$request2['file2'] 				= $data['file2'];
+		if(isset($data['idno'])) 				$request2['identity_no'] 		= $data['idno'];
+		
+		if(isset($request2)){
+			$request2['user_id'] 	= $id;
+			$userdetailid			= $data['userdetailid'];
+
+			if($userdetailid==''){
+				$userdetaildata = $this->db->insert('users_detail', $request2);
+			}else{
+				$userdetaildata = $this->db->update('users_detail', $request2, ['id' => $userdetailid]);
+			}
+		}
+
+
+		if(isset($data['address'])) 			$request3['address'] 		= $data['address'];
+		if(isset($data['province'])) 			$request3['province'] 		= $data['province'];
+		if(isset($data['city'])) 				$request3['city'] 			= $data['city'];		
+		if(isset($data['suburb'])) 				$request3['suburb'] 		= $data['suburb'];
+		if(isset($data['postal_code'])) 		$request3['postal_code'] 	= $data['postal_code']; 
+		
+		if(isset($request3)){
+			$request3['user_id'] 	= $id;
+			$request3['type'] 		= '3'; 
+			$useraddressid			= $data['useraddressid'];
+
+			if($useraddressid==''){
+				$useraddressdata = $this->db->insert('users_address', $request3);
+			}else{
+				$useraddressdata = $this->db->update('users_address', $request3, ['id' => $useraddressid]);
+			}
+		}
+
+		
+		if(isset($data['bank_name'])) 				$request4['bank_name'] 		= $data['bank_name'];
+		if(isset($data['branch_code'])) 			$request4['branch_code'] 	= $data['branch_code'];
+		if(isset($data['account_name'])) 			$request4['account_name'] 	= $data['account_name'];	
+		if(isset($data['account_no'])) 				$request4['account_no'] 	= $data['account_no'];
+		if(isset($data['account_type'])) 			$request4['account_type'] 	= $data['account_type']; 
+		
+
+		if(isset($request4)){
+			$request4['user_id'] 	= $id;
+			$userbankid				= $data['userbankid'];
+
+			if($userbankid==''){
+				$userbankdata = $this->db->insert('users_bank', $request4);
+			}else{
+				$userbankdata = $this->db->update('users_bank', $request4, ['id' => $userbankid]);
+			}
+		}
+
+		/*if(isset($data['area']) && count($data['area'])){
+			$auditorids = array_column($data['area'], 'id');
+			//$this->db->where('user_id', $id)->where_not_in('id', $auditorids)->delete('users_auditor_area');
+
+			foreach($data['area'] as $key => $request5){
+				$request5['user_id'] = $id;
+
+				if($request5['id']==''){
+					$usersarea = $this->db->insert('users_auditor_area', $request5);
+				}else{
+					$usersarea = $this->db->update('users_auditor_area', $request5, ['id' => $request5['id']]);
+				}
+			}
+		}*/
+		// else{
+		// 	$this->db->where('user_id', $id)->delete('users_auditor_area');
+		// }
+		
+		if((!isset($userdata) && !isset($userdetaildata) && !isset($useraddressdata) && !isset($userbankdata) && !isset($usersarea)) && $this->db->trans_status() === FALSE)
+		{
+			$this->db->trans_rollback();
+			return false;
+		}
+		else
+		{
+			$this->db->trans_commit();
+			return true;
+		}
+	}
+
+	public function AreaAction($data){
+
+		$id = $data['user_id'];
+
+		if(isset($data['area']) && count($data['area'])){
+			$auditorids = array_column($data['area'], 'id');
+			foreach($data['area'] as $key => $request5){
+				$request5['user_id'] = $id;
+
+				// if($request5['id']==''){
+					$usersarea = $this->db->insert('users_auditor_area', $request5);
+				// }else{
+				// 	$usersarea = $this->db->update('users_auditor_area', $request5, ['id' => $request5['id']]);
+				// }
+			}
+		}
+	}
+
+	public function deleteAuditorArea($data){
+
+		$user_id 	= $data['user_id'];
+		$id 		= $data['area_id'];
+
+		$data = $this->db->where('id', $id)->delete('users_auditor_area');
+		return $id;
+
+	}
+
+	public function ChatgetList($type, $requestdata=[])
+	{
+		$this->db->select('c.*, concat(ud1.name, " ", ud1.surname) name');
+		$this->db->from('chat c');
+		$this->db->join('users_detail ud1', 'ud1.user_id = c.from_id', 'left');
+	
+		if(isset($requestdata['id']))		$this->db->where('c.id', $requestdata['id']);
+		if(isset($requestdata['cocid']))	$this->db->where('c.coc_id', $requestdata['cocid']);
+			
+		if(isset($requestdata['fromto'])){
+			$this->db->group_start();
+				$this->db->group_start();
+					$this->db->where('c.from_id', $requestdata['fromto']);
+					$this->db->where('c.state1', '1');
+				$this->db->group_end();
+				$this->db->or_group_start();
+					$this->db->where('c.to_id', $requestdata['fromto']);
+					$this->db->where('c.state2', '1');
+					$this->db->or_where('c.state2', '0');
+				$this->db->group_end();
+			$this->db->group_end();
+		}
+		
+		if(isset($requestdata['checkfrom'])){
+			$this->db->group_start();
+				$this->db->where('c.from_id', $requestdata['checkfrom']);
+				$this->db->where('c.state1', '0');
+			$this->db->group_end();
+		}
+		
+		if(isset($requestdata['checkto'])){
+			$this->db->group_start();
+				$this->db->where('c.to_id', $requestdata['checkto']);
+				$this->db->where('c.state2', '0');
+			$this->db->group_end();
+		}
+		
+		if(isset($requestdata['viewed'])){
+			$this->db->group_start();
+				$this->db->where('c.to_id', $requestdata['viewed']);
+				$this->db->where('c.viewed', '0');
+			$this->db->group_end();
+		}
+		
+		if(isset($requestdata['state'])){
+			$this->db->group_start();
+				$this->db->where('c.state1', '0');
+				$this->db->or_where('c.state2', '0');
+			$this->db->group_end();
+		}
+		
+		
+		if($type=='count'){
+			$result = $this->db->count_all_results();
+		}else{
+			$this->db->order_by('c.created_at', 'asc');
+			
+			$query = $this->db->get();
+			
+			if($type=='all') 		$result = $query->result_array();
+			elseif($type=='row') 	$result = $query->row_array();
+		}
+		
+		return $result;
+	}
+
 	public function getInvoiceList($type, $requestdata=[]){
 		
 		$this->db->select('inv.*, ud.name, ud.surname, ud.vat_vendor');
@@ -1238,7 +1538,6 @@ class Api_Model extends CC_Model
 		return $result1;
 	}
 
-
 	public function checkusers($data){
 
 		$email 		= trim($data['email']);
@@ -1251,22 +1550,34 @@ class Api_Model extends CC_Model
 		$this->db->where('u.type', $type);
 		$emailcount = $this->db->count_all_results();
 		if ($emailcount =='1') {
-			$this->db->select('u.id, u.email, u.password');
+			$this->db->select('u.id, u.email, u.password, u.formstatus');
 			$this->db->from('users u');
 			$this->db->where('u.email', $email);
-			$this->db->where('u.password', $password);
+			$this->db->where('u.formstatus', '1');
 			$this->db->where('u.type', $type);
-			$passwordcount = $this->db->count_all_results();
-			if ($passwordcount =='1') {
-				$status 	= '1';
-				$message 	= 'Profile verified on Audit-IT database.';
+			$formstatus = $this->db->count_all_results();
+			if ($formstatus =='1') {
+				$this->db->select('u.id, u.email, u.password');
+				$this->db->from('users u');
+				$this->db->where('u.email', $email);
+				$this->db->where('u.password', $password);
+				$this->db->where('u.type', $type);
+				$passwordcount = $this->db->count_all_results();
+				if ($passwordcount =='1') {
+					$status 	= '1';
+					$message 	= 'Profile verified on Audit-IT database.';
+				}else{
+					$status 	= '3';
+					$message 	= 'Entered password not match with Audit-IT database.';
+				}
 			}else{
-				$status 	= '3';
-				$message 	= 'Entered password not match with Audit-IT database.';
+				$status 		= '4';
+				$message 		= 'Profile not verified on Audit-IT database.';
 			}
+			
 		}else{
-			$status 		= '2';
-			$message 		= 'There is no matching emaill address on Audit-IT database.';
+			$status 			= '2';
+			$message 			= 'There is no matching emaill address on Audit-IT database.';
 		}
 
 		// $query = $this->db->where_in('type', $type)->get_where('users', ['email' => $email, 'password' => $password]);
