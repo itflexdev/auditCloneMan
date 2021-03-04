de8b90119f00e1d039583f9d2d3b0347485d1026
manikandanRJSM <rmanikandanrjsm@outlook.com>
2021-03-04 05:35:03 -0800

    api update

--- application/controllers/webservice/Api.php
+++ application/controllers/webservice/Api.php
@@ -2013,7 +2013,7 @@ class Api extends CC_Controller
 						$colorcode = '#50c6f2';
 					}
 
-					$jsonData['review_details'][] = [ 'reviewid' => $value['id'], 'reviewtype' => $this->config->item('reviewtype')[$value['reviewtype']], 'statementname' => $value['statementname'], 'colorcode' => $colorcode
+					$jsonData['review_details'][] = [ 'reviewid' => $value['id'], 'reviewtype' => $this->config->item('reviewtype')[$value['reviewtype']], 'statementname' => $value['statementname'], 'comments' => $value['comments'], 'colorcode' => $colorcode
 					];
 				}
 				$message 	= 'CoC Details';
