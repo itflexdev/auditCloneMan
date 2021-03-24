76becab54be89c11591a64828cdd3a922f801ac8
manikandanRJSM <rmanikandanrjsm@outlook.com>
2021-03-24 06:57:31 -0700

    card 126aV

--- application/controllers/company/dashboard/Index.php
+++ application/controllers/company/dashboard/Index.php
@@ -6,12 +6,248 @@ class Index extends CC_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('Company_Model');
+		$this->load->model('Company_allocatecoc_Model');
+        $this->load->model('Companyperformancedetails_Model');
+        $this->load->model('Companyperformance_Model');
 	}
 	
 	public function index()
 	{
-		$data['plugins']			= [];
-		$data['content'] 			= $this->load->view('company/dashboard/index', '', true);
+		$userid 					= $this->getUserID();
+		$userdata 					= $this->getUserDetails();
+		$result 					= $this->Company_Model->getList('row', ['id' => $userid], ['userscompany']);
+
+		$userorderstock  			= $this->Coc_Model->getCOCList('count', ['allocated_id' => $userid]);
+		$coccount					= $this->Coc_Model->COCcount(['user_id' => $userid]);
+		$cocdata 					= $this->Company_Model->getstockList('all', ['roletype' => '6', 'user_id' => $userid]);
+
+		foreach ($cocdata as $cocdatakey => $cocdatavalue) {
+			if ($cocdatavalue['coc_status'] == '4') {
+                    $nonloged[] = $cocdatavalue['id'];
+                }
+		}
+
+         $totalpoints = $this->Companyperformancedetails_Model->getList('all', ['user_id' => $userid, 'status' => ['1'], 'date' => date("Y-m-d")]);
+
+        $points = 0;
+        foreach ($totalpoints as $totalpoint) {
+            $points += $totalpoint['points'];
+        }
+
+        $myperformance                              = $this->myperformanceCalc(['compID' => $userid, 'province' => $userdata['province']]);
+        $score                                      = round((($myperformance['companyperformancepoints']+$myperformance['adminsettiingsnlm'])*($myperformance['otherplumber'])+($myperformance['adminsettiingslm']))*($myperformance['licensedplumber']));
+
+        $pagedata['performancestatus']              = '0';
+        $pagedata['nonlogcoc']						= isset($nonloged) ? count($nonloged) : '0';
+		$pagedata['coccount']		                = isset($coccount['count']) ? $coccount['count'] : '0';
+		$pagedata['adminstock']		                = isset($userorderstock) ? $userorderstock : '0';
+		$pagedata['id']				                = $userid;
+        // $pagedata['overallperformancestatus']       = '1';
+        // $pagedata['provinceperformancestatus1']     = '1';
+
+        $pagedata['countrytotal']                   = $myperformance['countrytotal'];
+        $pagedata['reginoaltotal']                  = $myperformance['reginoaltotal'];
+        $pagedata['pirbmsg']                  		= $result['message'];
+
+		$pagedata['overallperformancestatuslimit'] 	= $this->userperformancestatus(['overall' => '1', 'limit' => '3']);
+		$pagedata['provinceperformancestatus'] 		= $this->userperformancestatus(['province' => $userdata['province']]);
+		$pagedata['provinceperformancestatuslimit'] = $this->userperformancestatus(['province' => $userdata['province'], 'limit' => '3']);
+
+		$data['plugins']			= ['datatables','validation','datepicker','inputmask','select2', 'echarts'];
+		$data['content'] 			= $this->load->view('company/dashboard/index', (isset($pagedata) ? $pagedata : ''), true);
 		$this->layout2($data);
 	}
+
+
+	public function DTemplist()
+    {
+        $post           = $this->input->post();
+        $userdata       = $this->getUserDetails();
+        $totalcount     = $this->Company_Model->getEmpList('count', ['type' => '4', 'approvalstatus' => ['0', '1'], 'formstatus' => ['1'], 'status' => ['0', '1', '2']] + $post);
+        $results        = $this->Company_Model->getEmpList('all', ['type' => '4', 'approvalstatus' => ['0', '1'], 'formstatus' => ['1'], 'status' => ['0', '1', '2']] + $post);
+       
+        $companystatus  = $this->config->item('companystatus');
+        $rollingavg                 = $this->getRollingAverage();
+        $date                       = date('Y-m-d', strtotime(date('Y-m-d').'+'.$rollingavg.' months'));
+        
+        $totalrecord = [];
+        if (count($results) > 0) {
+            foreach ($results as $result) {
+
+                $userdatacoc_count = $this->Coc_Model->COCcount(['user_id' => $result['user_id']]);
+                $desigcount     = $this->Company_Model->getdesignationCount(['designation' => $result['designation']]);
+
+                $reginalranking       = $this->userperformancestatus12(['userid' => $result['user_id']]);
+                $nationalrangking      = $this->userperformancestatus12(['province' => $userdata['province'], 'userid' => $result['user_id']]);
+
+
+                $performance = $this->Plumber_Model->performancestatus('all', ['plumberid' => $result['user_id'], 'archive' => '0', 'date' => $date]);
+
+                $array_orderqty = $this->Company_allocatecoc_Model->getqty('row', ['user_id' => $result['user_id']]);
+        		$balace_coc 	= $array_orderqty['sumqty'];
+
+                $per_points = array_sum(array_column($performance, 'point'));
+
+                $points     = $this->Company_Model->cpdPoints($result['user_id']);
+
+                if ($points[0]['cpd']!=''){
+                     $points         = round($points[0]['cpd'],2);
+                }else{
+                    $points         = '0';
+                } 
+                if( $per_points!=''){
+                    $performance    = round($per_points,2);
+                }else{
+                    
+                    $performance    = '0';
+                }
+                if ($result['designation']=='6' || $result['designation']=='4') {
+                   $divclass = 'lm';
+                }else{
+                    $divclass = 'other';
+                }
+                $overall = round((number_format($points+$performance)/$desigcount[0]['desigcount']),1);
+                $companystatus1 = isset($companystatus[$result['status']]) ? $companystatus[$result['status']] : '';
+                
+                
+                $totalrecord[] = [
+                    'status'        => $this->config->item('plumberstatus')[$result['status']],
+                    'namesurname'   => $result['name'].' '.$result['surname'],
+                    'rating'        => '<input type="hidden" value="'.$nationalrangking.'" class="'.$divclass.'">'.$nationalrangking.'',
+                    'rating1'        => '<input type="hidden" value="'.$reginalranking.'" class="'.$divclass.'">'.$reginalranking.'',
+                ];
+            }
+        }
+       
+        $json = array(
+            "draw" => intval($post['draw']),
+            "recordsTotal" => intval($totalcount),
+            "recordsFiltered" => intval($totalcount),
+            "data" => $totalrecord,
+        );
+
+        echo json_encode($json);
+    }
+
+
+    public function userperformancestatus12($data = []){  
+        $rollingavg     = $this->getRollingAverage();
+        $userid         = (isset($data['userid'])) ? $data['userid'] : $this->getUserID();
+        $date           = date('Y-m-d', strtotime(date('Y-m-d').'+'.$rollingavg.' months'));
+        
+        $extradata              = $data;
+        $extradata['date']      = $date;
+        $extradata['archive']   = '0';
+        
+        if(count($data)==0){
+            $extradata['plumberid'] = $userid;
+        }elseif(count($data) > 0 && in_array('performancestatus', array_keys($data))){
+            unset($extradata['date']);
+            unset($extradata['archive']);
+            $extradata['plumberid'] = $userid;
+            unset($data);
+        }
+        
+        $results = $this->Plumber_Model->performancestatus('all', $extradata);
+         // return $results;
+        if(isset($data) && count($data) > 0){
+            if(isset($data['limit'])){
+                return $results;
+            }else{
+                $useridsearch = array_search($userid, array_column($results, 'userid'));
+                return ($useridsearch !== false) ? $useridsearch+1 : 0;
+            }
+        }else{
+            return count($results) ? array_sum(array_column($results, 'point')) : '0';
+        }
+    }
+
+    public function myperformanceCalc($data = []){
+        
+
+        $results        = $this->Company_Model->getEmpList('all', ['type' => '4', 'approvalstatus' => ['0', '1'], 'formstatus' => ['1'], 'status' => ['0', '1', '2'], 'comp_id' => $data['compID']]);
+        $rollingavg     		= $this->getRollingAverage();
+        $date           		= date('Y-m-d', strtotime(date('Y-m-d').'+'.$rollingavg.' months'));
+        $employeeTotalcount 	= count($results);
+
+        if (count($results) > 0) {
+            foreach ($results as $result) {
+                $desigcount                       = $this->Company_Model->getdesignationCount(['designation' => $result['designation']]);
+                $overallperformancestatus[]       = $this->userperformancestatus12(['userid' => $result['user_id']]);
+                $provinceperformancestatus[]      = $this->userperformancestatus12(['province' => $data['province'], 'userid' => $result['user_id']]);
+
+                $performance = $this->Plumber_Model->performancestatus('all', ['plumberid' => $result['user_id'], 'archive' => '0', 'date' => $date]);
+                $per_points[] = array_sum(array_column($performance, 'point'));
+                $points     = $this->Company_Model->cpdPoints($result['user_id']);
+
+                $per_points1 = array_sum(array_column($performance, 'point'));
+                if ($points[0]['cpd']!=''){
+                     $points         = round($points[0]['cpd'],2);
+                }else{
+                    $points         = '0';
+                } 
+                if( $per_points1!=''){
+                    $performance    = round($per_points1,2);
+                }else{
+                    
+                    $performance    = '0';
+                }
+                
+                $overall = round((number_format($points+$performance)/$desigcount[0]['desigcount']),1);
+
+                if ($result['designation']=='6' || $result['designation']=='4') {
+                    
+                   $lm[]        = $overall;
+                }else{
+                    $other[]    = $overall;
+                }
+
+            }
+        }
+
+         $totalpoints = $this->Companyperformancedetails_Model->getList('all', ['user_id' => $data['compID'], 'status' => ['1'], 'date' => date("Y-m-d")]);
+        $companyperformancepoints = 0;
+        foreach ($totalpoints as $totalpoint) {
+            $companyperformancepoints += $totalpoint['points'];
+        }
+
+        $document_types = $this->Companyperformance_Model->getList('all');
+
+        if (isset($overallperformancestatus)) {
+            $countrycumulative = array_sum($overallperformancestatus);
+            $countrycount = count($overallperformancestatus);
+
+            $countrytotal = round($countrycumulative/$countrycount);
+        }
+
+        if (isset($provinceperformancestatus)) {
+            $reginoalcumulative = array_sum($provinceperformancestatus);
+            $reginoalcount = count($provinceperformancestatus);
+
+            $reginoaltotal = round($reginoalcumulative/$reginoalcount);
+        }
+
+        if (isset($lm)) {
+            $lmCount = count($lm);
+            $lmTotal = number_format((float)(array_sum($lm)/$lmCount), 2, '.', '');
+        }
+
+        if (isset($other)) {
+            $otherCount = count($other);
+            $otherTotal = number_format((float)(array_sum($other)/$otherCount), 2, '.', '');
+        }
+
+        $datarray['countrytotal'] 	                = isset($countrytotal) ? $countrytotal : '0';
+        $datarray['reginoaltotal'] 	                = isset($reginoaltotal) ? $reginoaltotal : '0';
+        $datarray['myperformance'] 	                = isset($performance) ? $performance : '0';
+        $datarray['licensedplumber']                = isset($lmTotal) ? $lmTotal : '0';
+        $datarray['otherplumber']                   = isset($otherTotal) ? $otherTotal : '0';
+        $datarray['companyperformancepoints']       = isset($companyperformancepoints) ? $companyperformancepoints : '0';
+        $datarray['adminsettiingsnlm']              = $document_types[7]['points'];
+        $datarray['adminsettiingslm']               = $document_types[8]['points'];
+
+        return $datarray;
+        
+    }
 }
--- application/views/company/dashboard/index.php
+++ application/views/company/dashboard/index.php
@@ -1,3 +1,7 @@
+<?php 
+	$pdfimg 				= base_url().'assets/images/pdf.png';
+	$profileimg 			= base_url().'assets/images/profile.jpg';
+?>
 <div class="row page-titles">
 	<div class="col-md-5 align-self-center">
 		<h4 class="text-themecolor">Dashboard</h4>
@@ -16,8 +20,299 @@
 	<div class="col-12">
 		<div class="card">
 			<div class="card-body">
-				<p>Welcome</p>
+		
+		<style>
+			img.bar_profil {
+                width: 37% !important;
+                height: 60px !important;
+			}
+
+			img.bar_profil {
+                margin-left: 44px !important;
+            }
+
+			span.ver_name1 {
+    			left: -64px;
+    			top: 120px;
+			}
+
+			span.bar_bot {
+    			text-align: left !important;
+    			width: 44% !important;
+			}
+
+			span.ver_name2 {
+    			left: -64px;
+    			top: 60px;
+			}
+
+			span.ver_name3 {
+    			left: -64px;
+    			top: 12px;
+			}
+
+			.message_sec {
+    			padding: 24px 7px !important;
+			}
+
+			div#mycocs {
+    			top: 10px !important;
+				left: 60px !important;
+    			height: 474px !important;
+			}
+			
+			.text-themecolor {
+    			font-size: 20px;
+    			font-weight: 600;
+			}
+
+			@media only screen and (max-width: 767px) {
+
+			.mini-sidebar .left-sidebar, .mini-sidebar .sidebar-footer {
+    			left: -270px;
+			}
+
+			.page-wrapper {
+    			margin-left: 0px !important;
+			}
+
+			div#mycocs {
+    			left: 0px !important;
+			}
+
+			span.ver_name1 {
+    			left: -54px;
+    			top: 130px;
+			}
+
+			span.ver_name2 {
+    			left: -54px;
+    			top: 70px;
+			}
+
+			span.ver_name3 {
+    			left: -54px;
+    			top: 20px;
+			}
+
+			}
+
+			@media only screen and (min-width:768px) and (max-width: 1024px) {
+
+				.mini-sidebar .left-sidebar, .mini-sidebar .sidebar-footer {
+					left: -270px;
+				}
+
+				.page-wrapper {
+					margin-left: 0px !important;
+				}
+				
+				div#mycocs {
+					left: 0px !important;
+				}
+
+				.ver_name1 {
+    				left: -30px !important;
+    				top: 150px !important;
+				}
+
+				.ver_name2 {
+    				left: -30px !important;
+    				top: 90px !important;
+				}
+
+				.ver_name3 {
+    				left: -30px !important;
+    				top: 42px !important;
+				}
+
+				div#mycocs{
+					height:420px !important;
+				}
+			}
+
+			</style>
+				<div class="row">
+				<div class="col-md-6 coc_sectiom_cus">
+					<div class="coc_sec">
+						<p class="cus_my_coc">My COC’s</p>
+						<div id="mycocs" style="width:100%; height:400px;" class="cus_line"></div>
+					</div>
+				</div>
+
+				<div class="col-md-6 my_perform_sec displaynone">
+					<div class="cus_perform"> 
+						<p class="perf_hed">My Performance Status</p>
+						<div id="performancechart" style="width:100%; height:0px;"></div>
+						<div class="perform_graph">
+                         <img src="<?php echo base_url().'assets/images/graph_icon.png'; ?>">
+                        </div>
+						<p class="per_scr_hed">My Performance Score</p>	
+						<p class="per_scr_box"><?php echo $performancestatus; ?></p>
+						<div class="my_Rank">
+							<p class="per_coun_hed">My Country Ranking</p>
+							<p class="cus_co_rank"><?php echo $countrytotal; ?></p>
+						</div>
+						<div class="my_Rank">
+							<p class="per_coun_hed">My Regional  Ranking</p>
+							<p class="cus_co_rank"><?php echo $reginoaltotal; ?></p>
+						</div>
+					</div>
+				</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="col-md-12 message_sec displaynone">
+							<div class="cus_msg">
+								<p>My Pirb Messages</p>
+								<?php 
+									$data 	= $this->db->where("groups='4' AND status='1'")->get('messages')->result_array();
+									$msg 	= "";
+									foreach ($data as $key => $value) {
+										$currentDate = date('Y-m-d');
+										$startdate   = date('Y-m-d',strtotime($value['startdate']));
+										$enddate = date('Y-m-d',strtotime($value['enddate']));
+										if ($currentDate>= $startdate && $currentDate<=$enddate){
+											$msg = $msg.$value['message'].'</br></br>'; 
+										}
+									}
+									
+									echo '<div class="col-md-12">'.$msg.'</div>';
+								?>
+							</div>
+						</div>
+						<div class="table-responsive m-t-40">
+					<label for="name">Employee Listing</label>
+					<table class="table table-bordered table-striped datatables fullwidth">
+						<thead>
+							<tr>
+								<th>Status</th>
+								<th>Plumbers Name and Surname</th>
+								<th>Industry Ranking - Regional</th>
+								<th>Industry Ranking - National</th>
+							</tr>
+						</thead>
+					</table>
+				</div>
+						<div class="col-md-12 message_sec">
+							<div class="cus_msg">
+								<p>My Pirb Messages</p>
+								<?php 
+									echo '<div class="col-md-12">'.$pirbmsg.'</div>';
+								?>
+							</div>
+						</div>
+				
+						<div class="row">
+							<div class="col-md-6 cus_reg_sec">
+								<div class="cus_regt">
+									<p class="reg_he">Current Top 3 Regional Ranking (Country)</p>
+									<div class="row reg_grap">
+										<?php 
+											foreach($overallperformancestatuslimit as $key => $performance){ 
+												$userid					= $performance['userid'];
+												$filepath				= base_url().'assets/uploads/plumber/'.$userid.'/';
+												$file2 					= isset($performance['image']) ? $performance['image'] : '';
+												if($file2!=''){
+													$explodefile2 	= explode('.', $file2);
+													$extfile2 		= array_pop($explodefile2);
+													$photoidimg 	= (in_array($extfile2, ['pdf', 'tiff'])) ? $pdfimg : $filepath.$file2;
+													$photoidurl		= $filepath.$file2;
+												}else{
+													$photoidimg 	= $profileimg;
+													$photoidurl		= 'javascript:void(0);';
+												}
+										?>
+												<div class="col-md-4 cus_bar_">
+													<div class="bar_img">
+														<img src="<?php echo $photoidimg; ?>" class="bar_profil">
+														<span class="ver_name<?php echo $key+1; ?>"><?php echo $performance['name']; ?></span>
+														<img src="<?php echo base_url().'assets/images/bar'.($key+1).'.png'; ?>" class="bar_3d cus_pirb<?php echo $key+1; ?>">
+														<span class="bar_bot"><?php echo $performance['point']; ?></span>
+													</div>
+												</div>
+										<?php } ?>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 cus_reg_sec">
+								<div class="cus_regt">
+									<p class="reg_he">Current Top 3 Regional Ranking (Province)</p>
+									<div class="row reg_grap">
+										<?php 
+											foreach($provinceperformancestatuslimit as $key => $performance){ 
+												$userid					= $performance['userid'];
+												$filepath				= base_url().'assets/uploads/plumber/'.$userid.'/';
+												$file2 					= isset($performance['image']) ? $performance['image'] : '';
+												if($file2!=''){
+													$explodefile2 	= explode('.', $file2);
+													$extfile2 		= array_pop($explodefile2);
+													$photoidimg 	= (in_array($extfile2, ['pdf', 'tiff'])) ? $pdfimg : $filepath.$file2;
+													$photoidurl		= $filepath.$file2;
+												}else{
+													$photoidimg 	= $profileimg;
+													$photoidurl		= 'javascript:void(0);';
+												}
+										?>
+												<div class="col-md-4 cus_bar_">
+													<div class="bar_img">
+														<img src="<?php echo $photoidimg; ?>" class="bar_profil">
+														<span class="ver_name<?php echo $key+1; ?>"><?php echo $performance['name']; ?></span>
+														<img src="<?php echo base_url().'assets/images/bar'.($key+1).'.png'; ?>" class="bar_3d cus_pirb<?php echo $key+1; ?>">
+														<span class="bar_bot"><?php echo $performance['point']; ?></span>
+													</div>
+												</div>
+										<?php } ?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+<script>
+	$(function(){
+		var nonlogcoc 				= '<?php echo $nonlogcoc; ?>';
+		var adminstock 				= '<?php echo $adminstock; ?>';
+		var coccount 				= '<?php echo $coccount; ?>';
+		var userid 					= '<?php echo $id; ?>';
+
+		barchart2(
+			'mycocs',
+			{
+				xaxis : [
+					'Number of \n Non Logged \n COCs',
+					'COC’s yet \n to allocated',
+					'Permitted COCs \n that you are \n able to purchase'
+				],
+				series : [{
+					name : 'My COC’s',
+					yaxis : [
+						nonlogcoc,
+						adminstock,
+						coccount
+					],
+					colors : ['#C4E0B2','#CEF57F','#9ADD11']
+				}]
+			}
+		);
+
+		var options = {
+			url 	: 	'<?php echo base_url()."company/dashboard/index/DTemplist"; ?>',
+			data 	: {"comp_id": userid},
+			columns : 	[
+							{ "data": "status" },
+							{ "data": "namesurname" },
+							{ "data": "rating" },
+							{ "data": "rating1" },
+						],
+						
+		};
+		
+		ajaxdatatables('.datatables', options);
+	});
+</script>
