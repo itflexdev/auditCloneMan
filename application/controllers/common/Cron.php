4577d333506d7676e95d2d1a70b72929ce1a6b96
manikandanRJSM <rmanikandanrjsm@outlook.com>
2021-04-01 06:13:15 -0700

    issue cpd

--- application/controllers/common/Cron.php
+++ application/controllers/common/Cron.php
@@ -177,10 +177,7 @@ class Cron extends CC_Controller {
 								$array1 = ['{Plumbers Name and Surname}','{TODAYS DATE}', 'Points Table', '{plumbers registration renewal date}'];
 								$array2 = [$userQueryvalue['name_surname'], $currentDate, $cpdTable, date('m-d-Y', strtotime($userQueryvalue['expirydate']))];
 								$body = str_replace($array1, $array2, $template['email_body']);
-
-								if ($userQueryvalue['email'] == 'alselect@hotmail.com' || $userQueryvalue['email'] == 'info@articulateit.co.za' || $userQueryvalue['email'] == 'support@articulateit.co.za' || $userQueryvalue['email'] == 'steve.iopsa@webmail.co.za' || $userQueryvalue['email'] == 'lea@verconsult.co.za' || $userQueryvalue['email'] == 'lorrainem@iopsa.org' || $userQueryvalue['email'] == 'technical@iopsa.org') {
-									$this->CC_Model->sentMail($userQueryvalue['email'],$template['subject'],$body);
-								}
+								$this->CC_Model->sentMail($userQueryvalue['email'],$template['subject'],$body);
 								
 							}
 							$smsdata 	= $this->Communication_Model->getList('row', ['id' => '14', 'smsstatus' => '1']);
@@ -190,11 +187,7 @@ class Cron extends CC_Controller {
 								$smsbody1 = ['{total Points}','{total points required}', '{next registration date}'];
 								$smsbody2 = [$total, $totalDB, date('m-d-Y', strtotime($userQueryvalue['expirydate']))];
 								$sms = str_replace($smsbody1, $smsbody2, $smsdata['sms_body']);
-								
-								if ($userQueryvalue['email'] == 'alselect@hotmail.com' || $userQueryvalue['email'] == 'info@articulateit.co.za' || $userQueryvalue['email'] == 'support@articulateit.co.za' || $userQueryvalue['email'] == 'steve.iopsa@webmail.co.za' || $userQueryvalue['email'] == 'lea@verconsult.co.za' || $userQueryvalue['email'] == 'lorrainem@iopsa.org' || $userQueryvalue['email'] == 'technical@iopsa.org') {
-									$this->sms(['no' => $userQueryvalue['mobile_phone'], 'msg' => $sms]);
-								}
-								
+								$this->sms(['no' => $userQueryvalue['mobile_phone'], 'msg' => $sms]);
 							}
 
 							$plumberemails .= $userQueryvalue['email'].',';
